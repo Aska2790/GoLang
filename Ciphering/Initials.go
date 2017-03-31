a872849224a9af063eee5a660504f6d804ef1daf
Aska2790 <arslanannaev7@gmail.com>
2017-03-31 11:15:10 +0300

    1

---
+++ Ciphering/Initials.go
@@ -0,0 +1,53 @@
+/**
+ школа Ш++
+задание: Взятие инициалов полного имени
+автор: Арслан Аннаев
+*/
+
+
+package main
+
+import (
+	"bufio"
+	"os"
+	"fmt"
+	"strings"
+)
+
+
+
+func main() {
+	name := GetName("Введите полное имя : ")
+	init := Initials(name)
+	fmt.Println(init)
+}
+
+
+// Ввод ФИО
+func GetName(text string) string {
+	in := bufio.NewScanner(os.Stdin)
+	fmt.Print(text)
+	in.Scan()
+	if err := in.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Ошибка ввода:", err)
+	}
+	return in.Text()
+}
+
+
+// обработка строки
+func Initials (name string ) string {
+	var word string
+
+	if strings.HasSuffix(name, "-" ){				// заменять если имя составное и между стоит тире
+		strings.Replace(name, "-", " ", len(name))
+	}
+
+	str := strings.Fields(name)					// разбивка на отдельные слова
+
+	for i :=0; i < len(str); i++ {					// пройтисьм по словам взять перввй символ
+		k := string(str[i])
+		word += strings.ToUpper(string(k[0]))+"."		// перевести вверхний регистр и сконкатенировать
+	}
+	return word
+}
---
+++ Ciphering/VigenereChipher.go
@@ -0,0 +1,89 @@
+/**
+ школа Ш++
+задание: Шифр Видженера
+автор: Арслан Аннаев
+*/
+
+package main
+
+import (
+	"fmt"
+	"unicode"
+	"strings"
+	"bufio"
+	"os"
+)
+const (
+	UP_A = 65
+	LOW_a = 97
+	LET_COUNT = 26
+)
+var(
+	alphabet = make(map[string] int)
+	str = "abcdefghijklmnopqrstuvwxyz"
+)
+
+
+func main() {
+	key := GetStr("Enter the key")
+	text := GetStr("Enter the Text ")
+	fmt.Println(Encoder(text, key))
+}
+
+// инициализаци мапы для ключа
+func InitMap(){
+	for i :=0; i < LET_COUNT; i++{
+		alphabet[string (str[i])] = i+1
+	}
+}
+
+
+// шифр видженера
+func Encoder( text, key string) string{
+	InitMap()
+
+	var result = [] rune(text)
+	iterator := 0
+
+	for i:=0; i < len(text); i++{
+		if unicode.IsLetter (rune (text[i])){
+			if iterator >= len(key) {
+				iterator = 0
+			}
+			result[i] = Coders( text[i], byte(key[iterator]))
+			iterator++
+		}
+	}
+	bs := [] byte(string(result))
+	return string(bs)
+}
+
+
+
+
+// кодек для преобразования символа со смещением
+func Coders( UserLetter, KeysLetter byte) rune{
+	k := strings.ToLower(string(KeysLetter))
+	var key int = alphabet[string(k)]
+	var s rune
+
+	if unicode.IsUpper(rune(UserLetter)){				// если она верхнего регистра
+		s =   rune ( (((UserLetter -  UP_A )+ byte(key)) %LET_COUNT)+ UP_A)   // возвращаем букву со смещением
+
+	} else if unicode.IsLower(rune(UserLetter)){
+		s =  rune ( (((UserLetter -  LOW_a )+ byte(key)) %LET_COUNT)+ LOW_a)	// буква ниж рег со смещением
+	}
+	return s
+}
+
+
+// Get string to encrypt
+func GetStr(text string) string {
+	in := bufio.NewScanner(os.Stdin)
+	fmt.Println(text)
+	in.Scan()
+	if err := in.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Ошибка ввода:", err)
+	}
+	return in.Text()
+}
